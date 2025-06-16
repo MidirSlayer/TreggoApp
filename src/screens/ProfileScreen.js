093f9bf408545677115e155d0e128c0cedee403f
Oscar <xtheslayer361x@gmail.com>
2025-06-15 23:38:17 -0600

    depuraciuon de codigo no utilisado y uso de useFocusEffect para que los distintos items cargen cada ves que se abre la ventana del perfil

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,17 +1,10 @@
-import React, {useEffect, useState, useLayoutEffect, use, act} from "react";
-import { View, FlatList, StyleSheet, Alert, TouchableOpacity} from "react-native";
+import React, {useEffect, useState} from "react";
+import { useFocusEffect } from "@react-navigation/native";
+import { View, FlatList, StyleSheet} from "react-native";
 import { getSession } from "../services/session";
 import { obtenerMisTrabajos } from "../services/supabase";
-import { actualizarPerfil, crearPerfil, obtenerPerfil } from "../services/perfil";
-import { Ionicons } from '@expo/vector-icons'
-import PerfilForm from "../components/PerfilForm";
-import { subirImagenPerfil } from "../services/storage";
 import TrabajoCard from "../components/TrabajoCard";
 import Texto from "../components/Text";
-import Button from "../components/Button";
-import { useBackHandler } from '@react-native-community/hooks';
-import { asegurarCredito } from "../services/AsegurarCredito";
-
 
 export default function ProfileScreen({navigation}) {
     const [user,setUser] = useState(null)
@@ -19,79 +12,22 @@ export default function ProfileScreen({navigation}) {
     const [modoEditar, setModoEditar] = useState(false);
     const [perfil, setPerfil] = useState(null);
 
-
-    useEffect(() => {
+    useFocusEffect(
+        React.useCallback(() => {
         async function cargarPerfil() {
             const session = await getSession();
             setUser(session.user);
-
-            await asegurarCredito();
 
             const misTrabajos = await obtenerMisTrabajos(session.user.id)
             setTrabajos(misTrabajos)
 
         }
-
         cargarPerfil();
-    }, []);
-
-    useBackHandler(() => {
-        if(modoEditar) {
-            setModoEditar(false);
-            return true;
-        }
-
-        return false;
-    });
-
-    async function cargarDatos () {
-
-        const datos = await obtenerPerfil(user.id);
-        setPerfil(datos || {});
-        setModoEditar(true);
-    }
-
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerRight: () => (
-                <TouchableOpacity onPress={cargarDatos} style={{marginRight: 16}}>
-                    <Ionicons name="person-add" size={24} color="#007AFF" />
-                </TouchableOpacity>
-            )
-        })
-    }, [navigation, user])
+    }, [])
+    )
+    
 
     if (!user) return <Texto>Cargando...</Texto>
-
-    if (modoEditar) {
-        return (
-            <PerfilForm
-            initialData={perfil}
-            onSubmit={async (datos) => {
-
-                console.log('datos recibidos', datos)
-                let urlFinal = datos.avatar_url;
-
-                if (datos.avatar_url?.startsWith('file')) {
-                    urlFinal = await subirImagenPerfil(datos.avatar_url, user.id);
-                }
-
-                const datosFinales = {...datos, avatar_url: urlFinal};
-                const yaTienePerfil = perfil && Object.keys(perfil).length > 0;
-
-                if (yaTienePerfil) {
-                    await actualizarPerfil(user.id, datosFinales);
-                    Alert.alert('Perfil actializado')
-                } else {
-                    await crearPerfil(user.id, datosFinales);
-                    Alert.alert('perfil creado')
-                }
-
-                setModoEditar(false)
-            }}
-        />
-        )
-    }
 
     return (
         <View style={styles.container}>
