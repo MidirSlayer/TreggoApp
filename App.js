e4eb83f2d708abe88a3a5a85c05d6210c66fd07a
Oscar <xtheslayer361x@gmail.com>
2025-06-23 22:26:23 -0600

    añadido de Stripe

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native"
 import AuthStack from "./src/navigation/AuthStack"
 import AppStack from "./src/navigation/Appstack";
 import { getSession } from "./src/services/session";
+import { StripeProvider } from "@stripe/stripe-react-native";
 
 export default function App() {
 
@@ -19,7 +20,9 @@ export default function App() {
 
     if (loading) return null
   return (
+    <StripeProvider publishableKey="pk_test_51RbVfj4gAfXcTJ82QkUVrvsOcxRWH1yMAnkpMQfGxr3lYTn2P6POZagqJAry2XFniyYc3pjLrfwWV2zJB7GwxRfN00C8Mwa7CY" >
     <NavigationContainer>
       {session ? <AppStack/> : <AuthStack/>}
     </NavigationContainer>
+    </StripeProvider>
 )};
